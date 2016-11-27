1866a092a5d60a208c9e9c3b89cabfd2192fa016
mkuznecov <7473233@gmail.com>
2016-11-27 17:47:11 +0300

    Задание 3. Сделайте сценарий логина

--- test_smoke.py
+++ test_smoke.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 import pytest
+from selenium.webdriver.common.by import By
+
 
 def test_user_can_open_traning_page(app):
-    app.wd.get('http://www.software-testing.ru/trainings/')
+    app.wd.get('http://localhost/litecart/admin/')
+    app.wd.find_element(By.NAME, 'username').click()
+    app.wd.find_element(By.NAME, 'username').send_keys('admin')
+    app.wd.find_element(By.NAME, 'password').click()
+    app.wd.find_element(By.NAME, 'password').send_keys('admin')
+    app.wd.find_element(By.NAME, 'login').click()
 
